fe3f8566dc93c5dd01e131d24913a645beec242d
김시훈 <kimsihun0928@naver.com>
2026-04-24 10:26:58 +0900

    시간포멧추가

--- src/main/java/com/tenco/blog/model/Board.java
+++ src/main/java/com/tenco/blog/model/Board.java
@@ -1,7 +1,9 @@
 package com.tenco.blog.model;
 
+import com.tenco.blog.util.MyDateUtil;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 
 import java.sql.Timestamp;
 
@@ -21,5 +23,10 @@ public class Board {
     private String title;
     private String content;
     private Timestamp createdAt;
+
+    // createdAt -> 포맷하는 메서드 만들어보기
+    public String getTime() {
+        return MyDateUtil.timestampFormat(createdAt);
+    }
 
 }
